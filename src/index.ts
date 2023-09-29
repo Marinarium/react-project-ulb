ef689179d87a52aaef542ee149ff376f45c4a529
marinarium <schwmk@gmail.com>
2023-09-29 17:15:47 +0300

    add webpack-dev-server and env params for modes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { someFn } from './test';
 
 someFn(1);
+
+document.querySelector('.root').innerHTML = '<h1>Hello World!</h1>'
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
 export function someFn (arg: number): string {
-  console.log('Hello!', arg);
+  console.log('Hello!', arg, 38279437889374892349237);
   return 'Hello!'
 }
