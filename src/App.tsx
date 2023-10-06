77dcab625245946a3e58f27a7632d8e1ed6e6fba
marinarium <schwmk@gmail.com>
2023-10-06 16:37:00 +0300

    add react-router-dom, code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from "react-router-dom";
 import "./index.scss";
+import {AboutAsync} from "./pages/About/About.async";
+import {HomeAsync} from "./pages/Home/Home.async";
 
 const App = () => {
     return (
         <div className="app">
-            <Counter />
+            <nav className="nav">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </nav>
+            <main className="main">
+                <Suspense fallback={<p>Loading...</p>}>
+                <Routes>
+                    <Route path="/" element={<HomeAsync/>}/>
+                    <Route path="/about" element={<AboutAsync/>}/>
+                </Routes>
+                </Suspense>
+            </main>
         </div>
     );
 };
---
+++ src/pages/About/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores cumque laborum sunt ullam. Architecto
+            beatae delectus dolores modi molestias placeat totam veniam voluptate?
+        </div>
+    );
+};
+
+export default About;
