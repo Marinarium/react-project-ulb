54f4a8c7028e74565a7b795914ef350c59c4c164
marinarium <schwmk@gmail.com>
2023-10-11 22:03:46 +0300

    add babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,6 +9,26 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     }
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        },
+                    ],
+                ]
+            }
+        }
+    }
+
     const cssLoaders = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -45,6 +65,7 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 
     return [
         svgLoader,
+        babelLoader,
         typScriptLoader,
         cssLoaders,
         fileLoader,
